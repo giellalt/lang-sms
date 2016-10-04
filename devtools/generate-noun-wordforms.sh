41375b5f7fdcf8bff50d9fab60c9baf14b522741
Sjur N Moshagen <sjurnm@mac.com>
2016-10-04 09:34:48 +0000

    [Template merge - langs/und] Changed variable name and definition to allow overriding the path to the called script, to make it easy to use a locally modified script instead.

--- devtools/generate-noun-wordforms.sh
+++ devtools/generate-noun-wordforms.sh
@@ -7,16 +7,18 @@
 # Codes for the word forms to be generated - list as many or few as needed:
 morf_codes="+N+Sg+Nom \
             +N+Pl+Nom \
-            +N+Sg+Gen \
             +N+Sg+Ill \
             +N+Sg+Com \
-            +N+Pl+Gen \
+            +N+Sg+Gen \
+            +N+Pl+Acc \
             +N+Ess \
             +N+Der/Dimin+N+Sg+Nom \
-            +N+Der/Dimin+N+Pl+Nom \
             +N+Der/Dimin+N+Sg+Ill \
-            +N+Der/Dimin+N+Pl+Gen \
+            +N+Der/Dimin+N+Pl+Nom \
+            +N+Der/Dimin+N+Pl+Ill \
+            +N+Sg+Nom+PxSg3 \
             +N+Sg+Loc+PxSg3 \
+            +N+Sg+Acc+PxSg3 \
             +N+Ess+PxSg1 \
             +N+Ess+PxSg3"
 
@@ -30,13 +32,13 @@ exception_lexicons="(flagK)"
 generator_file=src/generator-dict-gt-norm
 
 # How many lemmas maximally for each lexicon:
-lemmacount=5000
+lemmacount=100
+
+# Specify path to the dir containing the script used for generation:
+script_dir=$GTCORE/scripts
 
 ################## DO NOT CHANGE BELOW HERE!!! ##################
-# Specify path to $GTCORE - we don't use Autotools for these scripts:
-giella_core=$GTCORE
-
-source $giella_core/scripts/generate-wordforms-for-cont_lexes.sh \
+source $script_dir/generate-wordforms-for-cont_lexes.sh \
         "$giella_core" \
         "$morf_codes" \
         "$source_file" \
--- devtools/generate-verb-wordforms.sh
+++ devtools/generate-verb-wordforms.sh
@@ -6,19 +6,16 @@
 ######### USER Variables - change these to your liking: #########
 # Codes for the word forms to be generated - list as many or few as needed:
 morf_codes="+V+Inf \
-            +V+Der/mõš+N+Sg+Nom \
-            +V+Der/mmuš+N+Sg+Nom \
-            +V+Der/NomAg+N+Sg+Nom"
-#            +V+Ind+Prs+Sg3 
-#            +V+Ind+Prs+Pl3 
-#            +V+Ind+Prt+Pl3 
-#            +V+Ind+Prs+ConNeg 
-#            +V+Pot+Sg3 
-#            +V+Cond+Sg3 
-#            +V+Imprt+Sg3 
-#            +V+Imprt+ConNegII 
-#            +V+Imprt+Pl3 
-#            +V+Der/škueʹtted+V+Inf
+            +V+Ind+Prs+Sg3 \
+            +V+Ind+Prs+Pl3 \
+            +V+Ind+Prt+Pl3 \
+            +V+Ind+Prs+ConNeg \
+            +V+Pot+Sg3 \
+            +V+Cond+Sg3 \
+            +V+Imprt+Sg3 \
+            +V+Imprt+ConNegII \
+            +V+Imprt+Pl3 \
+            +V+Der/škueʹtted+V+Inf"
 
 # Lexicon source file for lexicons and lemmas:
 source_file=src/morphology/stems/V_sms2x.lexc
@@ -30,13 +27,13 @@ exception_lexicons="(flagK)"
 generator_file=src/generator-dict-gt-norm
 
 # How many lemmas maximally for each lexicon:
-lemmacount=1000
+lemmacount=10
+
+# Specify path to the dir containing the script used for generation:
+script_dir=$GTCORE/scripts
 
 ################## DO NOT CHANGE BELOW HERE!!! ##################
-# Specify path to $GTCORE - we don't use Autotools for these scripts:
-giella_core=$GTCORE
-
-source $giella_core/scripts/generate-wordforms-for-cont_lexes.sh \
+source $script_dir/generate-wordforms-for-cont_lexes.sh \
         "$giella_core" \
         "$morf_codes" \
         "$source_file" \
