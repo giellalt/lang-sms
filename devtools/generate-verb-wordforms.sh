d8ea0e839ce4443e3bc7d6e1d2207cfa37f4d0af
Sjur N Moshagen <sjurnm@mac.com>
2016-10-04 08:44:42 +0000

    [Template merge - langs/und] Changed variable name in devtool scripts, to reflect similar changes elsewhere. Part of fixing bug #2219.

--- devtools/generate-verb-wordforms.sh
+++ devtools/generate-verb-wordforms.sh
@@ -6,19 +6,16 @@
 ######### USER Variables - change these to your liking: #########
 # Codes for the word forms to be generated - list as many or few as needed:
 morf_codes="+V+Inf \
-            +V+Der/mõš+N+Sg+Nom \
-            +V+Der/mmuš+N+Sg+Nom \
-            +V+Der/NomAg+N+Sg+Nom"
-#            +V+Ind+Prs+Sg3 
-#            +V+Ind+Prs+Pl3 
-#            +V+Ind+Prt+Pl3 
-#            +V+Ind+Prs+ConNeg 
-#            +V+Pot+Sg3 
-#            +V+Cond+Sg3 
-#            +V+Imprt+Sg3 
-#            +V+Imprt+ConNegII 
-#            +V+Imprt+Pl3 
-#            +V+Der/škueʹtted+V+Inf
+            +V+Ind+Prs+Sg3 \
+            +V+Ind+Prs+Pl3 \
+            +V+Ind+Prt+Pl3 \
+            +V+Ind+Prs+ConNeg \
+            +V+Pot+Sg3 \
+            +V+Cond+Sg3 \
+            +V+Imprt+Sg3 \
+            +V+Imprt+ConNegII \
+            +V+Imprt+Pl3 \
+            +V+Der/škueʹtted+V+Inf"
 
 # Lexicon source file for lexicons and lemmas:
 source_file=src/morphology/stems/V_sms2x.lexc
@@ -30,16 +27,14 @@ exception_lexicons="(flagK)"
 generator_file=src/generator-dict-gt-norm
 
 # How many lemmas maximally for each lexicon:
-lemmacount=1000
-
-# Specify path to $GTCORE - we don't use Autotools for these scripts:
-gtcore=$GTCORE
-
+lemmacount=10
 
 ################## DO NOT CHANGE BELOW HERE!!! ##################
-#source $gtcore/scripts/generate-wordforms-for-cont_lexes.sh \
-source $gtcore/scripts/generate-wordforms-for-cont_lexes.sh \
-        "$gtcore" \
+# Specify path to $GTCORE - we don't use Autotools for these scripts:
+giella_core=$GTCORE
+
+source $giella_core/scripts/generate-wordforms-for-cont_lexes.sh \
+        "$giella_core" \
         "$morf_codes" \
         "$source_file" \
         "$generator_file" \
