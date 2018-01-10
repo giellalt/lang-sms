d5acfcb6105ddada6124f02f7b4909f4d37775e2
leneantonsen <lene.antonsen@uit.no>
2018-01-10 12:02:20 +0000

    since there are continuation lexicons in root-file, I made an exeption for lines with ;

--- devtools/tag_test.sh
+++ devtools/tag_test.sh
@@ -3,7 +3,7 @@
 echo 'Are there tags not declared in root.lexc or misspelled?'
 cat src/morphology/clitics.lexc src/morphology/compounding.lexc  src/morphology/affixes/*lexc src/morphology/stems/*lexc |cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |tr -s ' ' |sed 's/^ //' | cut -d ' ' -f1 |sed 's/+/¢+/g' |sed 's/@/¢@/g'|tr '¢' '\n' | tr '#"0' '\n'| egrep '(\+|@)' |sort -u | egrep -v '^(\+|\+%|\+\/\-|\+Cmp\-|\+Cmp%\-|\@0)$' > lexctags
 
-cat src/morphology/root.lexc |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g'|sed 's/@/¢@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
+cat src/morphology/root.lexc |cut -d '!' -f1 | grep -v ';' |sed 's/+/¢+/g'|sed 's/@/¢@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
 
 echo 'Have a look at these:'
 
