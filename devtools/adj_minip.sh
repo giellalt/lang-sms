3ff457db72efdd3b8523b58afc2a6ca053fd1fce
Trond Trosterud <trond.trosterud@uit.no>
2021-04-09 16:29:54 +0200

    Add files for testing adjective declension.

---
+++ devtools/adj_minip.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# script to generate paradigms for generating word forms
+# command:
+# sh generate_contlex_para.sh PATTERN
+# example, when you are in sms:
+# sh devtools/adj_minip.sh HUMO  | less
+# sh devtools/adj_minip.sh hyeni 
+# Only get the lemma you ask for:
+# sh devtools/adj_minip.sh '^hyeni[:+]' 
+
+LOOKUP=$(echo $LOOKUP)
+GTLANGS=$(echo $GTLANGS)
+
+PATTERN=$1
+L_FILE="in.txt"
+cut -d '!' -f1 src/fst/stems/A_sms2x.lexc | egrep $PATTERN | tr '+' ':' | cut -d ':' -f1>$L_FILE
+
+P_FILE="test/data/testadjparadigm.txt"
+
+for lemma in $(cat $L_FILE);
+do
+ for form in $(cat $P_FILE);
+ do
+      echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-sms/src/generator-gt-norm.hfstol # hfst
+ done
+ rm -f $L_FILE
+done
+
