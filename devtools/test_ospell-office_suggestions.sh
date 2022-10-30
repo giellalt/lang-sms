giellalt/lang-est-x-plamk

[Template merge - langs/und] Corrected placeholder string for two-letter ISO language code.
git-svn-id: https://gtsvn.uit.no/langtech/trunk/experiment-langs/est@139246 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

devtools/test_ospell-office_suggestions.sh
@@ -8,7 +8,7 @@
 # set -x
 
 # Language being tested, ISO 639-1 code if available:
-GTLANG2=se
+GTLANG2=et
 
 # Directory variables:
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"

giellalt/lang-sms

devtools/test_ospell-office_suggestions.sh
@@ -8,7 +8,7 @@
 # set -x
 
 # Language being tested, ISO 639-1 code if available:
-GTLANG2=se
+GTLANG2=sms
 
 # Directory variables:
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"