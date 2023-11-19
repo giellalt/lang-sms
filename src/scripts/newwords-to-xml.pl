4461a31347a1dd8a3b8603b37526b5f2eafffc71
Trond Trosterud <trond.trosterud@uit.no>
2023-11-19 15:34:11 +0100

    docu

--- src/scripts/newwords-to-xml.pl
+++ src/scripts/newwords-to-xml.pl
@@ -1,9 +1,15 @@
 #!/usr/bin/perl -w
 use utf8 ;
 
-# Simple script to convert csv to xml
-# For input/outpus examples, see below.
-
+# Simple script to convert adhoc entries in *_newwords.lexc to *_sms2x.xml
+# Usage:
+# 1. Convert the *_newwords.lexc to a tabseparated list of
+#    lemma POS stem contlex trans, probably as follows (exchange "verbs" with other POS)
+#    grep ";" src/fst/stems/verbs_newwords.lexc|tr -s ' ' | tr '[+: ]' '\t' | tr -d '[;"]' > misc/xx_verbs.csv
+# 2. cat misc/xx_verbs.csv | perl src/scripts/newwords-to-xml.pl > misc/xx_verbs.xml
+# 3. Check the output and eventually change the script below
+# 4. Add the entries from misc/xx_verbs.xml to src/fst/stems/V_sms2x.xml and
+# 5. Remove the converted verbforms from verbs_newwords.lexc
 
 print STDOUT "<r>\n";
 
@@ -36,24 +42,4 @@ while (<>)
 
 print STDOUT "</r>\n";
 
-
-
-# Example input:
-#
-# aampumakenttä	N	skytefelt
-
-
-#Target output:
-#
-#   <e src="yr">
-#      <lg>
-#         <l pos="N">aampumakenttä</l>
-#      </lg>
-#      <mg>
-#         <tg>
-#            <t pos="N" gen="x">skytefelt</t>
-#         </tg>
-#      </mg>
-#   </e>
-#
 
