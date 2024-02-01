531a53f7498309f0b4224be9880cc0ca9eb1cbe4
Trond Tynnøl <tty001@uit.no>
2024-02-01 12:29:59 +0200

    Correct path i minip scripts

--- devtools/noun_minip.sh
+++ devtools/noun_minip.sh
@@ -16,7 +16,7 @@ GTLANGS=$(echo $GTLANGS)
 
 PATTERN=$1
 L_FILE="in.txt"
-cut -d '!' -f1 src/fst/stems/N_Kin_sms2x.lexc src/fst/stems/N_sms2x.lexc|egrep $PATTERN | tr '+' ':'| cut -d ':' -f1>$L_FILE
+cut -d '!' -f1 src/fst/morphology/stems/N_Kin_sms2x.lexc src/fst/morphology/stems/N_sms2x.lexc|egrep $PATTERN | tr '+' ':'| cut -d ':' -f1>$L_FILE
 
 P_FILE="test/data/testnounparadigm.txt"
 
@@ -24,7 +24,7 @@ for lemma in $(cat $L_FILE);
 do
  for form in $(cat $P_FILE);
  do
-   echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-sms/src/generator-gt-norm.hfstol
+   echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-sms/src/fst/generator-gt-norm.hfstol
  done
  rm -f $L_FILE
 done
--- devtools/verb_minip.sh
+++ devtools/verb_minip.sh
@@ -12,7 +12,7 @@ GTLANGS=$(echo $GTLANGS)
 
 PATTERN=$1
 L_FILE="in.txt"
-cut -d '!' -f1 src/fst/stems/V_*.lexc | egrep $PATTERN | cut -d ':' -f1>$L_FILE
+cut -d '!' -f1 src/fst/morphology/stems/V_*.lexc | egrep $PATTERN | cut -d ':' -f1>$L_FILE
 
 P_FILE="test/data/testverbparadigm.txt"
 
@@ -20,7 +20,7 @@ for lemma in $(cat $L_FILE);
 do
  for form in $(cat $P_FILE);
  do
-   echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-sms/src/generator-gt-norm.hfstol
+   echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-sms/src/fst/generator-gt-norm.hfstol
  done
  rm -f $L_FILE
 done
