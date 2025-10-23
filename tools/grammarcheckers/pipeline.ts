c3eba08ead18e28a55cd15375eee4ae26b528823
Sjur N Moshagen <sjurnm@mac.com>
2025-10-23 08:49:39 +0300

    Update divvun-runtime pipeline to mirror the old xml pipespec pipeline

--- tools/grammarcheckers/pipeline.ts
+++ tools/grammarcheckers/pipeline.ts
@@ -6,18 +6,15 @@ import { Command, StringEntry } from "./.divvun-rt/mod.ts";
 export default function smsGramRelease(entry: StringEntry): Command {
   let x = hfst.tokenize("tokenize", entry, { model_path: "tokeniser-gramcheck-gt-desc.pmhfst" });
   x = divvun.blanktag("whitespace", x, { model_path: "analyser-gt-whitespace.hfst" });
-  x = cg3.vislcg3("valency", x, { model_path: "valency.bin" });
   x = cg3.vislcg3("mwe-dis", x, { model_path: "mwe-dis.bin" });
   x = cg3.mwesplit("mwesplit", x);
-  x = divvun.blanktag("errorwhitespace", x, { model_path: "analyser-gt-errorwhitespace.hfst" });
   x = divvun.cgspell("speller", x, {
     acc_model_path: "acceptor.default.hfst",
     err_model_path: "errmodel.default.hfst",
   });
-  x = cg3.vislcg3("postspell-valency", x, { model_path: "valency-postspell.bin" });
   x = cg3.vislcg3("grc-disamb", x, { model_path: "grc-disambiguator.bin" });
   x = cg3.vislcg3("spell-sugg-filtering", x, { model_path: "spellchecker.bin" });
-  x = cg3.vislcg3("gramcheck", x, { model_path: "grammarchecker-release.bin" });
+  x = cg3.vislcg3("gramcheck", x, { model_path: "grammarchecker.bin" });
   return divvun.suggest("suggestions", x, { model_path: "generator-gramcheck-gt-norm.hfstol" });
 }
 
@@ -27,15 +24,12 @@ export default function smsGramRelease(entry: StringEntry): Command {
 export function localTest_dev(entry: StringEntry): Command {
   let x = hfst.tokenize("tokenize", entry, { model_path: "@./tokeniser-gramcheck-gt-desc.pmhfst" });
   x = divvun.blanktag("whitespace", x, { model_path: "@./analyser-gt-whitespace.hfst" });
-  x = cg3.vislcg3("valency", x, { model_path: "@../../src/cg3/valency.cg3" });
   x = cg3.vislcg3("mwe-dis", x, { model_path: "@../tokenisers/mwe-dis.cg3" });
   x = cg3.mwesplit("mwesplit", x);
-  x = divvun.blanktag("errorwhitespace", x, { model_path: "@./analyser-gt-errorwhitespace.hfst" });
   x = divvun.cgspell("speller", x, {
     acc_model_path: "@./acceptor.default.hfst",
     err_model_path: "@./errmodel.default.hfst",
   });
-  x = cg3.vislcg3("postspell-valency", x, { model_path: "@./valency-postspell.cg3" });
   x = cg3.vislcg3("grc-disamb", x, { model_path: "@./grc-disambiguator.cg3" });
   x = cg3.vislcg3("spell-sugg-filtering", x, { model_path: "@./spellchecker.cg3" });
   x = cg3.vislcg3("gramcheck", x, { model_path: "@./grammarchecker.cg3" });
